ebf2ae05021fd61d1e4f3c88f761ceb25e5dc2e8
dOKi <1793987313@qq.com>
2025-07-07 00:41:10 +0800

    修复导入路径，确保从正确的模块导入配置

--- scripts/cas.py
+++ scripts/cas.py
@@ -4,7 +4,7 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.chrome.options import Options
-from config import *
+from scripts.config import *
 import requests
 import time
 
